c28a8be077c34ac8e5aa43e227fb1a7c3a5a06e7
Thomas van den Bighelaar <tmsvdb80@gmail.com>
2017-09-15 16:28:05 +0200

    first commit

---
+++ email.php
@@ -0,0 +1,17 @@
+<?php
+  $message = 'name: ' . $_POST['name'] . "<br>" . 'email: ' . $_POST['email'] .  "<br><br>" . $_POST['message'];
+
+  $headers =  'MIME-Version: 1.0' . "\r\n";
+  $headers .= 'From: Your name <thomas@beardiegames.com>' . "\r\n";
+  $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+  $success = mail('thomas@beardiegames.com', 'contact message', $message, $headers);
+
+  if ($success) {
+   header("Location: /email_success.html");
+   exit;
+  } else {
+   header("Location: /email_error.html");
+   exit;
+  }
+?>
